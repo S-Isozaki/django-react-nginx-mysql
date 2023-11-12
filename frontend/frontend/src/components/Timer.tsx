3fae12376dcfca8f006a45046384e6d8f7a4d861
Isozaki Satoshi <i.satoshi1987@gmail.com>
2023-11-13 00:04:19 +0900

    enabled to stop the watch when input has completed.

--- frontend/frontend/src/components/Timer.tsx
+++ frontend/frontend/src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { elapsedTimeState, isRunningState } from '../recoil/Atom';
 import { useRecoilState } from 'recoil';
 
@@ -7,16 +7,17 @@ interface TimerProps {
 }
 const Timer: React.FC<TimerProps> = ({ onTimeChange }) => {
     const [elapsedTime, setElapsedTime] = useRecoilState(elapsedTimeState);
+    const [isRunning, setIsRunning] = useRecoilState(isRunningState);
+    const intervalRef = useRef(0);
     useEffect(() => {
         onTimeChange(elapsedTime);
     }, [elapsedTime])
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const nextElapsedTime = elapsedTime + 10;
-            setElapsedTime(nextElapsedTime);
-        }, 10);
-        return () => clearInterval(interval);
-    }, [elapsedTime])
+    clearInterval(intervalRef.current)
+    intervalRef.current = window.setInterval(() => {
+        const nextElapsedTime = elapsedTime + 10;
+        setElapsedTime(nextElapsedTime);
+    }, 10)
+    if(!isRunning) clearInterval(intervalRef.current);
     return (
         <p>{Math.floor(elapsedTime / (1000 * 60 * 60))}:{Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60))}:{Math.floor((elapsedTime % (1000 * 60)) / 1000)}:{elapsedTime % 1000}</p>
     )
